72ef07d9aac18481db9daef76c5ce0f42979dd9b
ccabreraumich <89881165+ccabreraumich@users.noreply.github.com>
2022-12-04 19:28:15 -0500

    Add files via upload

--- js/vehicle_modals.js
+++ js/vehicle_modals.js
@@ -1,27 +1,10 @@
 /* START modal code: copied/modified from: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_modal_img */
 // Get the modal
 var modal = document.getElementById("myModal");
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-// var img_mod1 = document.getElementsByClassName("img_mod");
-// var img_mod2 = document.getElementById("img_mod2");
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
 var img_caption = document.getElementsByTagName("figcaption")[0].innerHTML;
-// img.onclick = function(){
-//   modal.style.display = "block";
-//   modalImg.src = this.src;
-//   captionText.innerHTML = img_caption;
-//   console.log(img_caption);
-// }
 
-// img_mod1.addEventListener("click", displayMod);
-// img_mod2.addEventListener("click", displayMod);
-
-// const images = document.getElementsByClassName("img_mod");
-
-// Array.from(images).forEach(function(image) {
-// });
 
 var elements = document.getElementsByTagName("figure"); //christine modified from reference
 
